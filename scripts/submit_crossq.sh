16d7db180bf5f9518bf0b0c6a646006bfeac8ce9
Daniel Palenicek <daniel.palenicek@tu-darmstadt.de>
2024-05-03 11:15:50 +0200

    final reprod

--- scripts/submit_crossq.sh
+++ scripts/submit_crossq.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for env in 'Ant-v4' 'HalfCheetah-v4' 'Walker2d-v4' 'Hopper-v4' 'Humanoid-v4' 'HumanoidStandup-v4'; do
+for env in 'Ant-v4' 'Humanoid-v4'; do
     ENV=$env ALGO='CrossQ' sbatch slurm_experiment.sh;
     # ENV=$env ALGO='SAC' sbatch slurm_experiment.sh;
 done
